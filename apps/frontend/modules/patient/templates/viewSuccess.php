df0ff5eca9ee7f5b5a6137683c5f894cdeb5e191
GBO <gbo@F38.(none)>
2011-11-28 23:04:27 +0100

    GBO: 2011-11-28

--- apps/frontend/modules/patient/templates/viewSuccess.php
+++ apps/frontend/modules/patient/templates/viewSuccess.php
@@ -3,11 +3,11 @@
 <?php include_partial('header', array('defaultSearch' => $defaultSearch)); ?>
 
 <div class="patient">
-<h3>Patient</h3>
+<h2>Patient</h2>
 View patient: <?php echo $patient->firstname ?> <?php echo $patient->lastname ?>
 </div>
 
-<div class="events" style="border: 1px solid black; margin-left: 300px;">
+<div class="events">
 <h2>Events</h2>
 
 <div class="event">
@@ -23,12 +23,40 @@ View patient: <?php echo $patient->firstname ?> <?php echo $patient->lastname ?>
 </form>
 </div>
 
+
+<form class="event">
+<div class="field">
+<label for="event_title">Title</label>
+<input type="text" class="title" id="event_title" name="event[title]" />
+</div>
+<div class="field">
+<label for="event_date">Date</label>
+<input type="text" class="date" id="event_date" name="event[date]" />
+</div>
+<div class="field">
+<label for="event_consultant">Consultant</label>
+<select class="consultant" id="event_consultant" name="event[consultant]" >
+<option/>
+</select>
+</div>
+<div class="field">
+<label for="event_description">Description</label>
+<textarea name="event[description]" class="description" id="event_description"></textarea>
+</div>
+</form>
+
+
 <?php foreach ($events as $event) { ?>
 <div class="event">
-<span class="date"><?php echo $event->date ?></span>
-<span class="title"><?php echo $event->title ?></span>
-<span class="description"><?php echo $event->description ?></span>
+<a href="">
+<h3 class="title"><?php echo $event->title ?></h3>
+<span class="date">
+<span class="day"><?php echo date('d-M', $event->date) ?></span>
+<span class="year"><?php echo date('Y', $event->date) ?></span>
+</span>
 <span class="consultant"><?php echo $event->consultant_id ?></span>
+<div class="description"><?php echo $event->description ?></div>
+</a>
 </div>
 <?php } ?>
 
